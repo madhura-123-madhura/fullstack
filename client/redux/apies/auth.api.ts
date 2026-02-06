e4eef3bd52a2fa5583f66e399b9abc6b5d913fb5
madhura <madhuragulwelkar35@gmail.com>
2026-02-06 09:37:47 +0530

    initial commit

---
+++ client/redux/apies/auth.api.ts
@@ -0,0 +1,46 @@
+import { User } from "@/types/User"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const authApi = createApi({
+    reducerPath: "authApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth`,
+        credentials: "include"
+    }),
+    tagTypes: [""],
+    endpoints: (builder) => {
+        return {
+
+            register: builder.mutation<void, User>({
+                query: registerData => {
+                    return {
+                        url: "/signup",
+                        method: "POST",
+                        body: registerData
+                    }
+                },
+            }),
+            login: builder.mutation<void, User>({
+                query: loginData => {
+                    return {
+                        url: "/signin",
+                        method: "POST",
+                        body: loginData
+                    }
+                },
+            }),
+            logout: builder.mutation<void, User>({
+                query: lgoutData => {
+                    return {
+                        url: "/signout",
+                        method: "POST",
+                        body: lgoutData
+                    }
+                },
+            }),
+
+        }
+    }
+})
+
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation } = authApi
